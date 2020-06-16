cf0859d818e607623e73252c0bfa6a23c888a88a
Peter Schmid <peter.schmid@mirai-solutions.com>
2020-06-16 15:59:56 +0200

    really closes #1

--- R/app_server.R
+++ R/app_server.R
@@ -6,8 +6,6 @@
 #' @importFrom graphics hist
 #' @noRd
 app_server <- function(input, output, session) {
-
   callModule(mod_faithful_histogram_server, "hist-waiting", "waiting")
   callModule(mod_faithful_histogram_server, "hist-eruptions", "eruptions")
-
 }
